c55f0cb64e862fdb52fda06225ef0e148974c066
James <jamesaubudi@gmail.com>
2019-06-15 13:31:50 -0700

    ts react

---
+++ ts_react/README.md
@@ -0,0 +1,3 @@
+# My boilerplate for a basic Typescript React project
+
+## Use yarn run dev to run the webpack server
---
+++ ts_react/src/components/Hello.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const Hello = () => {
+    return (
+        <h1>This is a Typescript Boilerplate.</h1>
+    )
+}
